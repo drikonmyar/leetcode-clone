bac4907776f6ffa27a3778a067d5a378380e9b96
Nabyendu Ojha <nabyenduojha99@gmail.com>
2026-01-18 02:54:12 +0530

    added authentication with github

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>
-        Click me
-      </Button>
+      <UserButton />
     </div>
   );
 }
